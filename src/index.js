0f0ae6655f198d4e05f1bc8bbd00640269a17dcc
chrislemus <icrislemus@gmail.com>
2021-04-19 15:49:34 -0400

    added redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './styles/index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
 import reportWebVitals from './test/reportWebVitals';
+import rootReducer from './reducers/index';
 
+let store = createStore(rootReducer);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
