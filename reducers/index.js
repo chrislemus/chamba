e637171eefe008ed551547634ce83e6fdd0d4ff5
chrislemus <icrislemus@gmail.com>
2021-06-04 11:03:47 -0400

    remove connected router dependancy

--- reducers/index.js
+++ reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux';
-import { connectRouter } from 'connected-react-router';
 import userReducer from './userReducer';
 import authUserReducer from './authUserReducer';
 import alertModalReducer from './alertModalReducer';
