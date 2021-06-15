f667ad7870838031bd4bc7d733551fede9e4a39e
chrislemus <icrislemus@gmail.com>
2021-06-15 17:55:18 -0400

    replace customer index style from Bulma to Material UI

--- pages/dashboard/customers/index.js
+++ pages/dashboard/customers/index.js
@@ -2,9 +2,23 @@ import Link from 'next/link';
 import { useState } from 'react';
 import { useQuery } from 'react-query';
 import DataFetchWrapper from '../../../components/DataFetchWrapper';
-import Pagination from '../../../components/Pagination';
 import { fetchCustomers } from '../../../services/api';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Avatar from '@material-ui/core/Avatar';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import {
+  Box,
+  Button,
+  Typography,
+  IconButton,
+  TablePagination,
+} from '@material-ui/core';
 import {
   faArrowRight,
   faEdit,
@@ -23,130 +37,132 @@ export default function Customers() {
   const customers = data?.customers;
   const queryData = data?.queryData;
 
-  const displayCustomers = () => {
-    if (customers) {
-      return customers.map((customerInfo) => (
-        <CustomerListCard
-          customerInfo={customerInfo}
-          key={`customer-${customerInfo.id}`}
-        />
-      ));
-    }
-  };
-
   return (
     <>
-      <div className="app-header">
-        <div className="app-header-left">
-          <h1>Customers</h1>
-        </div>
-        <div className="app-header-right">
+      <Box display="flex" mb={5}>
+        <Box flexGrow={1}>
+          <Typography variant="h4">
+            <strong>Customers</strong>
+          </Typography>
+        </Box>
+        <Box>
           <Link href="/dashboard/customers/new">
-            <button className="button is-primary is-rounded">
+            <Button color="primary" variant="contained">
               Add Customer
-            </button>
+            </Button>
           </Link>
-        </div>
-      </div>
+        </Box>
+      </Box>
 
-      <div className="card ">
-        <header className="card-header  is-shadowless	pt-4 px-5">
-          <div className="field">
-            <p className="control has-icons-left">
-              <input
-                className="input"
-                type="text"
-                onChange={({ target }) => setQuery(target.value)}
-                placeholder="Search customers"
-              />
-
-              <span className="icon is-small is-left">
-                <FontAwesomeIcon icon={faSearch} />
-              </span>
-            </p>
-          </div>
-        </header>
+      <Box bgcolor="white" boxShadow={2} borderRadius={3} py={6} px={5}>
+        <Box mb={3}>
+          <TextField
+            onChange={({ target }) => setQuery(target.value)}
+            variant="outlined"
+            placeholder="Search customers"
+            size="small"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <FontAwesomeIcon icon={faSearch} />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Box>
         <DataFetchWrapper
           status={status}
           dataName={'Customers'}
           hasData={customers?.length > 0}
         >
           <>
-            <table className="table is-hoverable mt-5 is-fullwidth ">
-              <thead>
-                <tr>
-                  <th className="pl-5">Name</th>
-                  <th className="is-hidden-mobile">Email</th>
-                  <th className="is-hidden-mobile">Phone</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Email</TableCell>
+                  <TableCell>Phone</TableCell>
+                  <TableCell>Actions</TableCell>
+                </TableRow>
+              </TableHead>
 
-              <tbody>{displayCustomers()}</tbody>
-            </table>
-            <Pagination
-              page={page}
-              pageLimit={pageLimit}
-              setPage={setPage}
-              setPageLimit={setPageLimit}
-              totalPages={queryData?.totalPages}
+              <TableBody>
+                {customers &&
+                  customers.map(
+                    ({
+                      id,
+                      fullName,
+                      email,
+                      phoneMobile,
+                      phoneHome,
+                      avatar,
+                    }) => (
+                      <TableRow key={'client' + id}>
+                        <TableCell>
+                          <Box display="flex" alignItems="center">
+                            <Avatar src={avatar}>
+                              {fullName
+                                .split(' ')
+                                .map((name) => name[0])
+                                .join('')
+                                .toUpperCase()}
+                            </Avatar>
+                            <Box pl={2}>
+                              <Link href={`/dashboard/customers/${id}`}>
+                                {fullName}
+                              </Link>
+                            </Box>
+                          </Box>
+                        </TableCell>
+                        <TableCell>
+                          {email?.length ? (
+                            <a href={`mailto:${email}`} target="_blank">
+                              {email}
+                            </a>
+                          ) : (
+                            '--'
+                          )}
+                        </TableCell>
+                        <TableCell>
+                          {phoneMobile || phoneHome || '--'}
+                        </TableCell>
+                        <TableCell size="small">
+                          <Box
+                            display="flex"
+                            justifyContent="space-between"
+                            width="min-content"
+                          >
+                            <Link href={`/dashboard/customers/${id}/edit`}>
+                              <IconButton size="small" color="primary">
+                                <FontAwesomeIcon icon={faEdit} />
+                              </IconButton>
+                            </Link>
+                            <Box width="1.5em" />
+                            <Link href={`/dashboard/customers/${id}`}>
+                              <IconButton size="small" color="primary">
+                                <FontAwesomeIcon icon={faArrowRight} />
+                              </IconButton>
+                            </Link>
+                          </Box>
+                        </TableCell>
+                      </TableRow>
+                    )
+                  )}
+              </TableBody>
+            </Table>
+
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25]}
+              component="div"
+              page={page - 1}
+              rowsPerPage={pageLimit}
+              count={queryData?.results}
+              onChangePage={(_, page) => setPage(page + 1)}
+              onChangeRowsPerPage={({ target }) => setPageLimit(target.value)}
             />
           </>
         </DataFetchWrapper>
-      </div>
+      </Box>
     </>
   );
 }
-
-const CustomerListCard = (props) => {
-  const { id, fullName, email, phoneMobile, phoneHome, avatar } =
-    props.customerInfo;
-  const avatarLink =
-    avatar || 'https://bulma.io/images/placeholders/128x128.png';
-  const phone = phoneMobile || phoneHome;
-  return (
-    <tr>
-      <td className="pl-5">
-        <div className="columns is-mobile level">
-          <div className="column is-narrow">
-            <div className="has-background-info is-rounded-40 is-flex is-justify-content-center is-align-items-center">
-              <p className="has-text-white has-text-centered has-text-weight-bold">
-                {fullName
-                  .split(' ')
-                  .map((name) => name[0])
-                  .join('')
-                  .toUpperCase()}
-              </p>
-            </div>
-          </div>
-          <div className="column is-narrow">
-            <Link href={`/dashboard/customers/${id}`}>{fullName}</Link>
-          </div>
-          <div className="column " />
-        </div>
-      </td>
-      <td className="is-hidden-mobile">
-        {!!email?.length ? (
-          <a href={`mailto:${email}`} target="_blank">
-            {email}
-          </a>
-        ) : (
-          '--'
-        )}
-      </td>
-      <td className="is-hidden-mobile">{phone ? phone : '--'}</td>
-      <td>
-        <Link href={`/dashboard/customers/${id}/edit`}>
-          <button className="button is-ghost ">
-            <FontAwesomeIcon icon={faEdit} />
-          </button>
-        </Link>
-        <Link href={`/dashboard/customers/${id}`}>
-          <button className="button is-ghost ">
-            <FontAwesomeIcon icon={faArrowRight} />
-          </button>
-        </Link>
-      </td>
-    </tr>
-  );
-};
